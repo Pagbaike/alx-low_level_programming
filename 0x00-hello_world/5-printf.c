135709261b302527eedc4f7f787f1cbb3f911f29
pagbaike <princeagbaike524@gmail.com>
2022-12-08 19:31:05 -0800

    c printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main-Entry
+ *
+ * Return:Always 0
+ *
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of the art,\n");
+	return (0);
+}
