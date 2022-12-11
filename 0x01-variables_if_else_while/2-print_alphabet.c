cba734facb390d2bb16624a04d1165cd35bced68
pagbaike <princeagbaike524@gmail.com>
2022-12-11 10:14:33 -0800

    c game

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -14,16 +14,14 @@ int main(void)
         {
             if (m > n)
             {
-                putchar(n);
-                putchar(m);
+                printf("%c%c", n, m);
                 if (n != 56 || m != 57)
                 {
-                    putchar(',');
-                    putchar(' ');
+                    printf(", ");
                 }
             }
         }
     }
-    putchar('\n');
+    printf("\n");
     return (0);
 }
