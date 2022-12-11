fd833c7cf287d38017e924542e3ecec4a371e353
pagbaike <princeagbaike524@gmail.com>
2022-12-11 10:00:59 -0800

    c game

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,26 +2,27 @@
 
 /**
  * main - prints all possible different combinations of two digits
- * Return: ALways 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int n, m;
+    int n, m;
 
-	for (n = 48; n <= 56; n++)
-	{
-		for (m = 49; m <= 57; m++)
-		{
-			if (m > n)
-			{
-				putchar(n, m);
-				if (n != 56 || m != 57)
-				{
-					putchar(',', ' ');
-				}
-			}
-		}
-	}
-	putchar('\n');
-	return (0);
+    for (n = 48; n <= 56; n++)
+    {
+        for (m = 49; m <= 57; m++)
+        {
+            if (m > n)
+            {
+                printf("%c%c", n, m);
+                if (n != 56 || m != 57)
+                {
+                    putchar(',');
+                    putchar(' ');
+                }
+            }
+        }
+    }
+    putchar('\n');
+    return (0);
 }
